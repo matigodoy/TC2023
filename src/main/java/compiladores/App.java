2b4e8548fb19ffe186e0ee90afa91552d7d7917b
matias GODdoy <mgodoy@bitsion.com>
2023-05-23 20:35:49 -0300

    preparación correcta para la prueba

--- src/main/java/compiladores/App.java
+++ src/main/java/compiladores/App.java
@@ -10,7 +10,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         // System.out.println("Hello, Compilador!!!");
         // create a CharStream that reads from file
-        CharStream input = CharStreams.fromFileName("input/prog.txt");
+        CharStream input = CharStreams.fromFileName("input/entrada.txt");
 
         // create a lexer that feeds off of input CharStream
         compiladoresLexer lexer = new compiladoresLexer(input);
